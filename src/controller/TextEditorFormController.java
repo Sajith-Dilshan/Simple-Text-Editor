e1078d9476f8260b1eb9fbe652d38c5d45accb64
sajith <sajithd784@gmail.com>
2022-01-08 13:46:38 +0530

    create Application UI

--- src/controller/TextEditorFormController.java
+++ src/controller/TextEditorFormController.java
@@ -1,4 +1,84 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class TextEditorFormController {
+    public TextField txtFind;
+    public TextField txtReplace;
+
+    public void mnuitemNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemOpen_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemPrint_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemExit_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemCut_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemCopy_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemPaste_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemselectAll_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemFind_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemReplaceAll_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuitemAbout_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOpen_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnsave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCopy_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPaste_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRegExp_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCaseSensitive_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUp_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDown_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplaceAll_OnAction(ActionEvent actionEvent) {
+    }
 }
