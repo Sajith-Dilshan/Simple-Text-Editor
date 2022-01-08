33212039c6422ff2032f914e643af809e8d6675a
sajith <sajithd784@gmail.com>
2022-01-08 15:22:34 +0530

    link about form  to main Form

--- src/controller/TextEditorFormController.java
+++ src/controller/TextEditorFormController.java
@@ -1,7 +1,14 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class TextEditorFormController {
     public TextField txtFind;
@@ -43,7 +50,16 @@ public class TextEditorFormController {
     public void mnuitemReplaceAll_OnAction(ActionEvent actionEvent) {
     }
 
-    public void mnuitemAbout_OnAction(ActionEvent actionEvent) {
+    public void mnuitemAbout_OnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("/view/AboutForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Scene scene = new Scene(load);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("About page");
+        stage.setResizable(false);
+        stage.setMaximized(false);
+        stage.show();
     }
 
     public void btnNew_OnAction(ActionEvent actionEvent) {
